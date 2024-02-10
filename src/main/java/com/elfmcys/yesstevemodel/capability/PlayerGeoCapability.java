5d0622cb6e5c3a20b14c25b34f3937be57d82199
TomatoPuddin <tomato-puddin@outlook.com>
2024-02-10 16:28:43 +0800

    修复第一人称模组相关 molang 变量与 oculus 的兼容问题

--- src/main/java/com/elfmcys/yesstevemodel/capability/PlayerGeoCapability.java
+++ src/main/java/com/elfmcys/yesstevemodel/capability/PlayerGeoCapability.java
@@ -1,7 +1,10 @@
 package com.elfmcys.yesstevemodel.capability;
 
 import com.elfmcys.yesstevemodel.client.compat.FirstPersonCompat;
+import com.elfmcys.yesstevemodel.client.compat.IrisCompat;
+import com.elfmcys.yesstevemodel.client.entity.CustomPlayerEntity;
 import com.elfmcys.yesstevemodel.client.instance.CustomPlayerInstance;
+import com.elfmcys.yesstevemodel.geckolib3.core.event.predicate.AnimationEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
@@ -11,6 +14,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class PlayerGeoCapability extends CustomPlayerInstance {
+    private AnimationEvent<CustomPlayerEntity> lastEvent;
+
     public PlayerGeoCapability(AbstractClientPlayer player) {
         super(player, true);
     }
@@ -22,5 +27,33 @@ public class PlayerGeoCapability extends CustomPlayerInstance {
             return Minecraft.getInstance().options.getCameraType() != CameraType.FIRST_PERSON || !FirstPersonCompat.isInstalled() || !FirstPersonCompat.isEnabled();
         }
         return true;
+    }
+
+    private boolean shouldSkipUpdate() {
+        return animatable.getEntity() instanceof LocalPlayer && !canUpdateAsync() && IrisCompat.isInstalled() && IrisCompat.isRenderingShadow() && lastEvent != null;
+    }
+
+    @Override
+    public AnimationEvent<CustomPlayerEntity> syncUpdate(float partialTicks) {
+        if (shouldSkipUpdate()) {
+            return lastEvent;
+        }
+        return lastEvent = super.syncUpdate(partialTicks);
+    }
+
+    @Override
+    public AnimationEvent<CustomPlayerEntity> waitOrUpdate(float partialTicks) {
+        if (shouldSkipUpdate()) {
+            return lastEvent;
+        }
+        return lastEvent = super.waitOrUpdate(partialTicks);
+    }
+
+    @Override
+    public AnimationEvent<CustomPlayerEntity> waitForAsyncUpdate() {
+        if (shouldSkipUpdate()) {
+            return lastEvent;
+        }
+        return lastEvent = super.waitForAsyncUpdate();
     }
 }
