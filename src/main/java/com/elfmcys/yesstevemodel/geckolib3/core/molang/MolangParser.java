d173b33972e73748aa2a0b7eda023dd1ac27425e
TomatoPuddin <tomato-puddin@outlook.com>
2024-02-19 00:49:07 +0800

    改进 MolangParser 错误日志

--- src/main/java/com/elfmcys/yesstevemodel/geckolib3/core/molang/MolangParser.java
+++ src/main/java/com/elfmcys/yesstevemodel/geckolib3/core/molang/MolangParser.java
@@ -26,7 +26,7 @@ public class MolangParser {
         try {
             return parseExpressionUnsafe(molangExpression);
         } catch (Exception e) {
-            YesSteveModel.LOGGER.error("Failed to parse value " + molangExpression, e);
+            YesSteveModel.LOGGER.error("Failed to parse value \"{}\": {}", molangExpression, e.getMessage());
             return DoubleValue.ZERO;
         }
     }
