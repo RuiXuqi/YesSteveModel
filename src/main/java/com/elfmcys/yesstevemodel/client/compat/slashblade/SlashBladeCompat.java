a54058723dbbe619746f61861919f6f25bfc3b6e
tartaric_acid <baka943@qq.com>
2024-05-29 20:34:39 +0800

    添加对拔刀剑-重锋的兼容 - https://www.curseforge.com/minecraft/mc-mods/slashblade-resharped

--- src/main/java/com/elfmcys/yesstevemodel/client/compat/slashblade/SlashBladeCompat.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/slashblade/SlashBladeCompat.java
@@ -1,19 +1,17 @@
 package com.elfmcys.yesstevemodel.client.compat.slashblade;
 
-import net.minecraft.resources.ResourceLocation;
+import mods.flammpfeil.slashblade.item.ItemSlashBlade;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public class SlashBladeCompat {
-    private static final String SLASH_BLADE = "slashblade";
-    private static final ResourceLocation SLASH_BLADE_ID = new ResourceLocation(SLASH_BLADE, "slashblade");
+    private static final String SLASH_BLADE_ID = "slashblade";
 
     public static boolean isSlashBladeLoaded() {
-        return ModList.get().isLoaded(SLASH_BLADE);
+        return ModList.get().isLoaded(SLASH_BLADE_ID);
     }
 
     public static boolean isSlashBladeItem(ItemStack stack) {
-        return isSlashBladeLoaded() && SLASH_BLADE_ID.equals(ForgeRegistries.ITEMS.getKey(stack.getItem()));
+        return isSlashBladeLoaded() && stack.getItem() instanceof ItemSlashBlade;
     }
 }
--- src/main/java/com/elfmcys/yesstevemodel/client/compat/slashblade/SlashBladeRender.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/slashblade/SlashBladeRender.java
@@ -9,7 +9,7 @@ import mods.flammpfeil.slashblade.capability.slashblade.CapabilitySlashBlade;
 import mods.flammpfeil.slashblade.client.renderer.model.BladeModelManager;
 import mods.flammpfeil.slashblade.client.renderer.model.obj.WavefrontObject;
 import mods.flammpfeil.slashblade.client.renderer.util.BladeRenderState;
-import mods.flammpfeil.slashblade.init.SBItems;
+import mods.flammpfeil.slashblade.item.ItemSlashBlade;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.HumanoidArm;
@@ -20,13 +20,16 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class SlashBladeRender {
+    private static final ResourceLocation RESOURCE_DEFAULT_MODEL = new ResourceLocation("slashblade", "model/blade.obj");
+    private static final ResourceLocation RESOURCE_DEFAULT_TEXTURE = new ResourceLocation("slashblade", "model/blade.png");
+
     public static void renderSlashBlade(PoseStack matrixStack, MultiBufferSource bufferIn, int lightIn, ItemStack stack) {
         if (stack.isEmpty()) {
             return;
         }
         stack.getCapability(CapabilitySlashBlade.BLADESTATE).ifPresent(bladeState -> {
-            ResourceLocation texture = bladeState.getTexture().orElse(BladeModelManager.resourceDefaultTexture);
-            WavefrontObject obj = BladeModelManager.getInstance().getModel(bladeState.getModel().orElse(null));
+            ResourceLocation texture = bladeState.getTexture().orElse(RESOURCE_DEFAULT_TEXTURE);
+            WavefrontObject obj = BladeModelManager.getInstance().getModel(bladeState.getModel().orElse(RESOURCE_DEFAULT_MODEL));
             String part;
             if (bladeState.isBroken()) {
                 part = "blade_damaged";
@@ -41,7 +44,7 @@ public class SlashBladeRender {
     }
 
     public static void renderMainhandSlashBlade(LivingEntity livingEntity, GeoModelState model, PoseStack matrixStack, MultiBufferSource bufferIn, int lightIn, ItemStack stack, float partialTicks) {
-        if (stack.is(SBItems.slashblade)) {
+        if (stack.getItem() instanceof ItemSlashBlade) {
             matrixStack.pushPose();
             // 主手的刀渲染在左边
             if (!model.leftWaistBones().isEmpty()) {
@@ -58,8 +61,8 @@ public class SlashBladeRender {
                 return;
             }
             stack.getCapability(CapabilitySlashBlade.BLADESTATE).ifPresent(bladeState -> {
-                ResourceLocation texture = bladeState.getTexture().orElse(BladeModelManager.resourceDefaultTexture);
-                WavefrontObject obj = BladeModelManager.getInstance().getModel(bladeState.getModel().orElse(null));
+                ResourceLocation texture = bladeState.getTexture().orElse(RESOURCE_DEFAULT_TEXTURE);
+                WavefrontObject obj = BladeModelManager.getInstance().getModel(bladeState.getModel().orElse(RESOURCE_DEFAULT_MODEL));
                 String part;
                 if (bladeState.isBroken()) {
                     part = "blade_damaged";
@@ -83,7 +86,7 @@ public class SlashBladeRender {
     }
 
     public static void renderOffhandSlashBlade(GeoModelState model, PoseStack matrixStack, MultiBufferSource bufferIn, int lightIn, ItemStack stack) {
-        if (stack.is(SBItems.slashblade)) {
+        if (stack.getItem() instanceof ItemSlashBlade) {
             matrixStack.pushPose();
             // 副手的刀渲染在右边
             if (!model.rightWaistBones().isEmpty()) {
