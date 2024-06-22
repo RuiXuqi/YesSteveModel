98c01ffeede1686ab88ddc51879d839658986c40
tartaric_acid <baka943@qq.com>
2024-06-22 22:10:04 +0800

    删除冗余部分

--- src/main/java/com/elfmcys/yesstevemodel/geckolib3/core/molang/builtin/query/ItemRemainingDurability.java
+++ src/main/java/com/elfmcys/yesstevemodel/geckolib3/core/molang/builtin/query/ItemRemainingDurability.java
@@ -4,16 +4,11 @@ import com.elfmcys.yesstevemodel.geckolib3.core.molang.context.IContext;
 import com.elfmcys.yesstevemodel.geckolib3.core.molang.function.entity.LivingEntityFunction;
 import com.elfmcys.yesstevemodel.geckolib3.util.MolangUtils;
 import com.elfmcys.yesstevemodel.molang.runtime.ExecutionContext;
-import com.google.common.collect.Maps;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 
-import java.util.Map;
-
 public class ItemRemainingDurability extends LivingEntityFunction {
-    private static final Map<String, EquipmentSlot> MAPS = Maps.newHashMap();
-
     @Override
     protected Object eval(ExecutionContext<IContext<LivingEntity>> context, ArgumentCollection arguments) {
         EquipmentSlot equipmentSlot = MolangUtils.parseSlotType(context.entity(), arguments.getAsString(context, 0));
