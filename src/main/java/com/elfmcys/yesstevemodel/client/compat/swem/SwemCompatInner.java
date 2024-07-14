cfac5bd04e27f8b3b1921cdbf8fd1dfd7355c8ae
tartaric_acid <baka943@qq.com>
2024-07-14 19:21:43 +0800

    完善 swem 动画

--- src/main/java/com/elfmcys/yesstevemodel/client/compat/swem/SwemCompatInner.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/swem/SwemCompatInner.java
@@ -2,6 +2,7 @@ package com.elfmcys.yesstevemodel.client.compat.swem;
 
 import com.alaharranhonor.swem.forge.entities.horse.SWEMHorseEntityBase;
 import com.google.common.collect.Maps;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,8 +17,23 @@ public class SwemCompatInner {
     static String getAnimation(Player player) {
         if (player.getVehicle() instanceof SWEMHorseEntityBase horse) {
             SWEMHorseEntityBase.Gait gait = horse.getGait();
+            // 判断跳跃
+            double jumpHeight = horse.jumpHeight;
+            if (jumpHeight > 0) {
+                int jumpAnimIndex = Math.min(Mth.ceil(jumpHeight), 5) - 1;
+                return "swem:jump_lv" + (jumpAnimIndex + 1);
+            }
+            if (notMoving(horse)) {
+                return "swem:idle";
+            }
             return CACHE.computeIfAbsent(gait, g -> "swem:" + g.name().toLowerCase(Locale.ENGLISH));
         }
         return null;
+    }
+
+    static boolean notMoving(SWEMHorseEntityBase horse) {
+        double x = horse.getX() - horse.xo;
+        double z = horse.getZ() - horse.zo;
+        return Math.sqrt(x * x + z * z) <= 0d;
     }
 }
