229258c51dc5cc51c58eae73e6951cec46f1033e
tartaric_acid <baka943@qq.com>
2025-01-15 17:27:02 +0800

    修改音频播放机制，现在能支持自定义资源包里的音频了

--- src/main/java/com/elfmcys/yesstevemodel/geckolib3/core/event/SoundKeyframeExecutor.java
+++ src/main/java/com/elfmcys/yesstevemodel/geckolib3/core/event/SoundKeyframeExecutor.java
@@ -14,7 +14,6 @@ import com.elfmcys.yesstevemodel.init.ModSounds;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -39,20 +38,17 @@ public class SoundKeyframeExecutor {
             if (soundName.contains(":")) {
                 // 如果声音名带冒号，那么大概率就是调用原版音频，因为 Windows 中冒号不是合法的文件名
                 ResourceLocation soundId = new ResourceLocation(soundName);
-                SoundEvent soundEvent = ForgeRegistries.SOUND_EVENTS.getValue(soundId);
-                if (soundEvent != null) {
-                    MinecraftSoundInstance instance = new MinecraftSoundInstance(soundEvent, animatable.getEntity());
-                    cachePlaySounds.add(instance);
-                    Minecraft.getInstance().getSoundManager().play(instance);
-                    nextIndex++;
-                }
+                SoundEvent soundEvent = SoundEvent.createVariableRangeEvent(soundId);
+                MinecraftSoundInstance instance = new MinecraftSoundInstance(soundEvent, animatable.getEntity());
+                cachePlaySounds.add(instance);
+                Minecraft.getInstance().getSoundManager().play(instance);
             } else {
                 // 否则认为是自定义的音频文件
                 CustomSoundInstance instance = new CustomSoundInstance(ModSounds.CUSTOM.get(), soundName, animatable.getEntity());
                 cachePlaySounds.add(instance);
                 Minecraft.getInstance().getSoundManager().play(instance);
-                nextIndex++;
             }
+            nextIndex++;
         }
     }
 
