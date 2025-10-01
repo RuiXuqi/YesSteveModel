62b1277ec2a579a4a439e604ca525eda39a952e0
TomatoPuddin <tomato-puddin@outlook.com>
2025-10-01 17:29:26 +0800

    改进 top 信息

--- src/main/java/com/elfmcys/yesstevemodel/client/compat/top/TopPlugin.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/top/TopPlugin.java
@@ -2,6 +2,8 @@ package com.elfmcys.yesstevemodel.client.compat.top;
 
 import com.elfmcys.yesstevemodel.YesSteveModel;
 import com.elfmcys.yesstevemodel.capability.ModelInfoCapabilityProvider;
+import com.elfmcys.yesstevemodel.capability.VehicleModelInfoCapabilityProvider;
+import com.elfmcys.yesstevemodel.model.ServerModelManager;
 import com.elfmcys.yesstevemodel.network.NetworkHandler;
 import mcjty.theoneprobe.api.*;
 import net.minecraft.network.chat.Component;
@@ -10,6 +12,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
@@ -33,8 +36,23 @@ public final class TopPlugin implements Function<ITheOneProbe, Void> {
             if (entity instanceof ServerPlayer player) {
                 player.getCapability(ModelInfoCapabilityProvider.MODEL_INFO_CAP).ifPresent(cap -> {
                     if (cap.isMandatory() || NetworkHandler.isPlayerChannelPresent(player)) {
-                        probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
-                                .text(Component.translatable("top.yes_steve_model.model_info.id").append(cap.getModelId()));
+                        ServerModelManager.getModel(cap.getModelId()).ifPresent(m -> {
+                            probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
+                                    .text(Component.translatable("top.yes_steve_model.model_info.id").append(
+                                            StringUtils.defaultIfBlank(m.info().metadata() == null ? "" : m.info().metadata().name(), cap.getModelId())));
+                        });
+                    }
+                });
+            } else {
+                entity.getCapability(VehicleModelInfoCapabilityProvider.CAP).ifPresent(cap -> {
+                    if (cap.isInitialized()) {
+                        ServerModelManager.getModel(cap.getOwnerModelId())
+                                .filter(m -> m.vehicleModels().contains(entity.getType().builtInRegistryHolder().key().location()))
+                                .ifPresent(m -> {
+                                    probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
+                                            .text(Component.translatable("top.yes_steve_model.model_info.id").append(
+                                                    StringUtils.defaultIfBlank(m.info().metadata() == null ? "" : m.info().metadata().name(), cap.getOwnerModelId())));
+                                });
                     }
                 });
             }
