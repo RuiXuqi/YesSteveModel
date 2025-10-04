5eca013bc5b724ab7d6e475f431132f107fc5890
tartaric_acid <baka943@qq.com>
2025-10-04 17:17:50 +0800

    升级版本 - 修正仅客户端运行时，在跨服传送时可能会导致的崩溃问题

--- src/main/java/com/elfmcys/yesstevemodel/client/event/LocalPlayerRespawnEvent.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/event/LocalPlayerRespawnEvent.java
@@ -2,6 +2,7 @@ package com.elfmcys.yesstevemodel.client.event;
 
 import com.elfmcys.yesstevemodel.YesSteveModel;
 import com.elfmcys.yesstevemodel.capability.PlayerAnimatableCapabilityProvider;
+import com.elfmcys.yesstevemodel.network.NetworkHandler;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,7 +12,13 @@ import net.minecraftforge.fml.common.Mod;
 public class LocalPlayerRespawnEvent {
     @SubscribeEvent
     public static void onFire(ClientPlayerNetworkEvent.Clone event) {
-        if (!YesSteveModel.isAvailable()) return;
+        if (!YesSteveModel.isAvailable()) {
+            return;
+        }
+        // 仅客户端运行时，执行后续的 copyFrom 会出现 null 值问题
+        if (!NetworkHandler.isRemoteChannelPresent()) {
+            return;
+        }
 
         event.getOldPlayer().reviveCaps();
         event.getOldPlayer().getCapability(PlayerAnimatableCapabilityProvider.CAP).ifPresent(oldCap ->
