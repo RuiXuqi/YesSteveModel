94d4d1a100dba13317addd9c0770576753b73b98
tartaric_acid <baka943@qq.com>
2025-11-29 18:37:22 +0800

    修正与 Better combat 的兼容问题

--- src/main/java/com/elfmcys/yesstevemodel/client/compat/bettercombat/BetterCombatCompat.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/bettercombat/BetterCombatCompat.java
@@ -1,7 +1,9 @@
 package com.elfmcys.yesstevemodel.client.compat.bettercombat;
 
+import com.elfmcys.yesstevemodel.client.compat.bettercombat.event.PlayerAttackEvent;
 import com.elfmcys.yesstevemodel.client.entity.CustomPlayerEntity;
 import com.elfmcys.yesstevemodel.util.PersonView;
+import net.bettercombat.api.client.BetterCombatClientEvents;
 import net.minecraftforge.fml.loading.LoadingModList;
 
 public class BetterCombatCompat {
@@ -10,6 +12,9 @@ public class BetterCombatCompat {
 
     public static void init() {
         INSTALLED = LoadingModList.get().getModFileById(MOD_ID) != null;
+        if (INSTALLED) {
+            BetterCombatClientEvents.ATTACK_START.register(new PlayerAttackEvent());
+        }
     }
 
     public static boolean isInstalled() {
---
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/bettercombat/event/PlayerAttackEvent.java
@@ -0,0 +1,15 @@
+package com.elfmcys.yesstevemodel.client.compat.bettercombat.event;
+
+import net.bettercombat.api.AttackHand;
+import net.bettercombat.api.client.BetterCombatClientEvents;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.InteractionHand;
+
+public class PlayerAttackEvent implements BetterCombatClientEvents.PlayerAttackStart {
+    @Override
+    public void onPlayerAttackStart(LocalPlayer player, AttackHand hand) {
+        player.swingTime = -1;
+        player.swinging = true;
+        player.swingingArm = hand.isOffHand() ? InteractionHand.OFF_HAND : InteractionHand.MAIN_HAND;
+    }
+}
