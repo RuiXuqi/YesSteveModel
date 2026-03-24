7d17fb06c7f886f8015fbc67074e713113984eb9
tartaric_acid <baka943@qq.com>
2026-03-24 15:28:30 +0800

    修正女仆骑乘时垂直位移的部分问题

--- src/main/java/com/elfmcys/yesstevemodel/client/compat/touhoulittlemaid/client/render/CustomYsmMaidRenderer.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/touhoulittlemaid/client/render/CustomYsmMaidRenderer.java
@@ -3,26 +3,21 @@ package com.elfmcys.yesstevemodel.client.compat.touhoulittlemaid.client.render;
 import com.elfmcys.yesstevemodel.capability.VehicleAnimatableCapabilityProvider;
 import com.elfmcys.yesstevemodel.client.compat.touhoulittlemaid.capability.YsmMaidCapabilityProvider;
 import com.elfmcys.yesstevemodel.client.compat.touhoulittlemaid.client.CustomYsmMaidEntity;
-import com.elfmcys.yesstevemodel.client.renderer.replace.EntityRendererReplace;
 import com.elfmcys.yesstevemodel.geckolib3.core.event.predicate.AnimationEvent;
 import com.elfmcys.yesstevemodel.geckolib3.geo.GeoReplacedEntityRenderer;
-import com.elfmcys.yesstevemodel.geckolib3.model.GeoModelState;
 import com.elfmcys.yesstevemodel.geckolib3.model.provider.data.EntityModelData;
-import com.elfmcys.yesstevemodel.geckolib3.util.RenderUtils;
 import com.github.tartaricacid.touhoulittlemaid.entity.item.EntityBroom;
+import com.github.tartaricacid.touhoulittlemaid.entity.item.EntityChair;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.geckolib3.geo.GeoLayerRenderer;
 import com.github.tartaricacid.touhoulittlemaid.geckolib3.geo.IGeoEntity;
 import com.github.tartaricacid.touhoulittlemaid.geckolib3.geo.IGeoEntityRenderer;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -77,13 +72,28 @@ public class CustomYsmMaidRenderer extends GeoReplacedEntityRenderer<EntityMaid,
         // 如果女仆待命状态，需要下移三格
         if (maid.isMaidInSittingPose()) {
             poseStack.translate(0, -0.5, 0);
+            return;
         }
+
+        // 抱起女仆需要额外的偏移
         if (maid.getVehicle() instanceof Player) {
-            // 抱起女仆需要额外的偏移
             poseStack.translate(-0.05, 0.19, 0.24);
+            return;
         }
+
+        // 扫帚为了做到玩家也能兼容，所以需要和玩家偏移同步
         if (maid.getVehicle() instanceof EntityBroom) {
-            // 扫帚为了做到玩家也能兼容，所以需要和玩家偏移同步
+            poseStack.translate(0, -0.5, 0);
+            return;
+        }
+
+        // 坐垫不需要下移
+        if (maid.getVehicle() instanceof EntityChair) {
+            return;
+        }
+
+        // 如果女仆骑乘的实体没有 cap，那么应该下移三格，避免悬空
+        if (maid.getVehicle() != null && !maid.getVehicle().getCapability(VehicleAnimatableCapabilityProvider.CAP).isPresent()) {
             poseStack.translate(0, -0.5, 0);
         }
     }
