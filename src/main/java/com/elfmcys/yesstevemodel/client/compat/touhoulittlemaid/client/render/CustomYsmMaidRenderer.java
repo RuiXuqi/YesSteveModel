327784e2b481755bb9f1452b9b78c3cf70834ad4
tartaric_acid <baka943@qq.com>
2026-04-08 23:17:59 +0800

    部分模型更新 - 迷你酒狐、圣女酒狐、精灵酒狐、幸存者酒狐 - K 螺模型 - 修正女仆使用 ysm 在娱乐方块上遁地问题

--- src/main/java/com/elfmcys/yesstevemodel/client/compat/touhoulittlemaid/client/render/CustomYsmMaidRenderer.java
+++ src/main/java/com/elfmcys/yesstevemodel/client/compat/touhoulittlemaid/client/render/CustomYsmMaidRenderer.java
@@ -6,24 +6,30 @@ import com.elfmcys.yesstevemodel.client.compat.touhoulittlemaid.client.CustomYsm
 import com.elfmcys.yesstevemodel.geckolib3.core.event.predicate.AnimationEvent;
 import com.elfmcys.yesstevemodel.geckolib3.geo.GeoReplacedEntityRenderer;
 import com.elfmcys.yesstevemodel.geckolib3.model.provider.data.EntityModelData;
-import com.github.tartaricacid.touhoulittlemaid.entity.item.EntityBroom;
-import com.github.tartaricacid.touhoulittlemaid.entity.item.EntityChair;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.geckolib3.geo.GeoLayerRenderer;
 import com.github.tartaricacid.touhoulittlemaid.geckolib3.geo.IGeoEntity;
 import com.github.tartaricacid.touhoulittlemaid.geckolib3.geo.IGeoEntityRenderer;
+import com.github.tartaricacid.touhoulittlemaid.init.InitEntities;
 import com.mojang.blaze3d.vertex.PoseStack;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Set;
 
 public class CustomYsmMaidRenderer extends GeoReplacedEntityRenderer<EntityMaid, CustomYsmMaidEntity> implements IGeoEntityRenderer<EntityMaid> {
+    /**
+     * 屎山代码 +1，因为船和矿车有特殊偏移，需要修正
+     */
+    private static final Set<EntityType> OFFSET_FIX_ENTITIES = Set.of(EntityType.MINECART, EntityType.BOAT, InitEntities.BROOM.get());
     protected final List<GeoLayerRenderer<EntityMaid, CustomYsmMaidRenderer>> maidLayers = new ObjectArrayList<>();
 
     public CustomYsmMaidRenderer(EntityRendererProvider.Context context) {
@@ -75,25 +81,17 @@ public class CustomYsmMaidRenderer extends GeoReplacedEntityRenderer<EntityMaid,
             return;
         }
 
+        Entity vehicle = maid.getVehicle();
+
         // 抱起女仆需要额外的偏移
-        if (maid.getVehicle() instanceof Player) {
+        if (vehicle instanceof Player) {
             poseStack.translate(-0.05, 0.19, 0.24);
             return;
         }
 
-        // 扫帚为了做到玩家也能兼容，所以需要和玩家偏移同步
-        if (maid.getVehicle() instanceof EntityBroom) {
-            poseStack.translate(0, -0.5, 0);
-            return;
-        }
-
-        // 坐垫不需要下移
-        if (maid.getVehicle() instanceof EntityChair) {
-            return;
-        }
-
-        // 如果女仆骑乘的实体没有 cap，那么应该下移三格，避免悬空
-        if (maid.getVehicle() != null && !maid.getVehicle().getCapability(VehicleAnimatableCapabilityProvider.CAP).isPresent()) {
+        //特定的没有 cap 的实体应该下移三格，避免悬空
+        if (vehicle != null && OFFSET_FIX_ENTITIES.contains(vehicle.getType())
+            && !vehicle.getCapability(VehicleAnimatableCapabilityProvider.CAP).isPresent()) {
             poseStack.translate(0, -0.5, 0);
         }
     }
